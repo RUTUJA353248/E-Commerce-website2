0746f1c8c82682f6e787b14abb2c6d311f058548
Rutuja Nale <rutujanale0000@gmail.com>
2025-01-07 19:34:08 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+// Add any JavaScript functionality if needed (e.g., interactive buttons, image sliders, etc.)
+document.querySelector('.cta').addEventListener('click', () =>  {
+   
+    alert('Thank you for your interest! The product has been added to your cart.');
+    
+});
+//second button 
+
+document.querySelector('.cta-2').addEventListener('click', () =>  {
+   
+    alert('Thank you for your interest! The product has been added to your cart.');
+    
+});
+//third button
+document.querySelector('.cta-3').addEventListener('click', () =>  {
+   
+    alert('Thank you for your interest! The product has been added to your cart.');
+    
+});
+//forth button
+document.querySelector('.cta-4').addEventListener('click', () =>  {
+   
+    alert('Thank you for your interest! The product has been added to your cart.');
+    
+});
+//fifth button
+document.querySelector('.cta-5').addEventListener('click', () =>  {
+   
+    alert('Thank you for your interest! The product has been added to your cart.');
+    
+});
